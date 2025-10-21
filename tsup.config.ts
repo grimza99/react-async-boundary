1565999dad1883675b7868ac017984c6e6b28ea3
grimza99 <gbtmxlf@naver.com>
2025-10-22 02:23:34 +0900

    feat: react 및 react-dom에 대한 종속성 추가 및 구성 수정

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,10 @@
+// @ts-nocheck
 import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
-  external: ["react"],
+  external: ["react", "react-dom"],
   clean: true,
 });
