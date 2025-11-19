5ab0e1ecb6ae288e3a1b7fb8e8a5c3efe65a1de8
grimza99 <gbtmxlf@naver.com>
2025-11-19 16:27:11 +0900

    feat: 테스트 유틸리티 추가 - RenderWithBoundary 및 TestAsyncComponent 구현

---
+++ __tests__/utils/mswHandlers.ts
@@ -0,0 +1,24 @@
+import { http, HttpResponse } from "msw";
+
+export const SUCCESS_URL = "/api/success";
+export const NETWORK_ERROR_URL = "/api/network-error";
+export const AUTHORIZATION_ERROR_URL = "/api/code-404";
+
+export const mswHandlers = [
+  /**success */
+  http.get(SUCCESS_URL, () => {
+    return HttpResponse.json({
+      name: "grimza99",
+      date: `오늘은 ${new Date()}입니다.`,
+    });
+  }),
+  /**network error */
+  http.get(NETWORK_ERROR_URL, () => {
+    //network error
+    return HttpResponse.error();
+  }),
+  /**authorization error */
+  http.get(AUTHORIZATION_ERROR_URL, () => {
+    return new HttpResponse(null, { status: 404 });
+  }),
+];
